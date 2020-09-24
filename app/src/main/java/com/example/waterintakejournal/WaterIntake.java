de5b7da1e83b3c90801804c68e565278bc31fd63
Damika Sulaksen <damikadesilva@gmail.com>
2020-09-24 06:33:33 +0530

    Text WIJ

---
+++ app/src/main/java/com/example/waterintakejournal/WaterIntake.java
@@ -0,0 +1,34 @@
+package com.example.waterintakejournal;
+
+public class WaterIntake {
+    private String date;
+    private String time;
+    private String numGlass;
+
+    public WaterIntake() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getNumGlass() {
+        return numGlass;
+    }
+
+    public void setNumGlass(String numGlass) {
+        this.numGlass = numGlass;
+    }
+}
